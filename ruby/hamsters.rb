fd145845a95654e36fd993a4d207847a595bcb85
Jeanette Corona <jscarlettcorona@gmail.com>
2016-07-13 13:14:10 -0400

    Add hamster program

---
+++ ruby/hamsters.rb
@@ -0,0 +1,26 @@
+puts "What is the hamster's name?"
+
+name = gets.chomp
+
+puts "Volume level (1-10)"
+
+volume = gets.chomp
+
+puts "Fur color"
+
+furcolor = gets.chomp
+
+puts "Good candidate for adoption? (y/n)"
+
+candidate = gets.chomp
+
+puts "Estimated age?"
+
+age = gets.chomp
+
+if age == ""
+  age = nil
+else
+end
+
+puts age
