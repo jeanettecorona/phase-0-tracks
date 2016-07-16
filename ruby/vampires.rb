43f06c8075c73dca8ee2180f92e1b1638f078094
Jeanette Corona <jscarlettcorona@gmail.com>
2016-07-16 12:40:38 -0400

    Add questions for vampire interview

---
+++ ruby/vampires.rb
@@ -0,0 +1,18 @@
+# NEW EMPLOYEE INTERVIEW - WEREFOLF, INC
+
+# Ask questions
+
+puts "What is your name?"
+	name = gets.chomp
+
+puts "How old are you?"
+	age = gets.chomp
+
+puts "What year were you born?"
+	year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+	health_insurance = gets.chomp
