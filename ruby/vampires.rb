5aa45a5e799745bc3c4d04a949eca107491de41a
Jeanette Corona <jscarlettcorona@gmail.com>
2016-07-18 08:07:20 -0400

    Process multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,8 +1,13 @@
 
 # NEW EMPLOYEE INTERVIEW - WEREFOLF, INC
 
-# Ask questions
+# Ask how many employees will be processed
 
+puts "How many employees will be processed?"
+	employees = gets.to_i
+
+# Ask questions
+while employees > 0
 puts "What is your name?"
 	name = gets.chomp
 
@@ -30,27 +35,31 @@ puts "Would you like to enroll in the company's health insurance?"
 		wrong_age = false
 	end
 
-	if garlic_bread = "yes" || garlic_bread = "y" || garlic_bread = "YES" || garlic_bread = "y"
+	if garlic_bread == "yes" || garlic_bread == "y" || garlic_bread == "YES" || garlic_bread == "y"
 		likes_garlic_bread = true
 	else
 		likes_garlic_bread = false
 	end
 
-	if health_insurance = "yes" || "y" || "YES" || "y"
+	if health_insurance == "yes" || health_insurance == "y" || health_insurance == "YES" || health_insurance == "y"
 		wants_insurance = true
 	else
 		wants_insurance = false
 	end
 
 # Print Results
-	if (likes_garlic_bread || wants_insurance) && !wrong_age
-		puts "Probably not a vampire."
-	elsif ( !likes_garlic_bread || !wants_insurance) && wrong_age
-		puts "Probably a vampire."	
-	elsif !likes_garlic_bread && !wants_insurance && !wrong_age
-		puts "Almost certainly a vampire."
-	elsif is_vampire == true
-		puts "Definitely a vampire."
-	else
-		puts "Results inconclusive."
-	end
+case
+when (likes_garlic_bread || wants_insurance) && !wrong_age
+	puts "Probably not a vampire."
+when ( !likes_garlic_bread || !wants_insurance) && wrong_age
+	puts "Probably a vampire."	
+when !likes_garlic_bread && !wants_insurance && !wrong_age
+	puts "Almost certainly a vampire."
+when is_vampire
+	puts "Definitely a vampire."
+else
+	puts "Results inconclusive."
+end
+
+employees -= 1
+end
