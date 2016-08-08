3998656805b1590d145c04e59a2c5521987d6eb7
Jeanette Corona <jscarlettcorona@gmail.com>
2016-08-08 09:15:04 -0700

    Start writing pseudocode for Release 1

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+// find the longest string within an array
+	// for each string in the array
+		// split the strints
+		// count how many letters are in each string
+
+function longestWord(str){
+
+}
