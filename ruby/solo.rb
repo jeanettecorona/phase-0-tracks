b0a34eb744e42bd5d50ee4bf42a6305a604a2a6a
Jeanette Corona <jscarlettcorona@gmail.com>
2016-08-18 21:24:41 -0700

    Pseudocode the sword design

---
+++ ruby/solo.rb
@@ -0,0 +1,15 @@
+# Design a Sword class
+
+# Initialize a new instance of the class
+
+# Write three attributes using at least two data types
+	# Make sure they vary from instance to instances
+	# 1) Type of material
+	# 2) Manufactured location
+	# 3) Date manufactured
+
+ # Write at least three methods, at least one takes an argument
+ 	# 1) Lights up at night
+ 	# 2) Extends in length
+ 	# 3) Folds into pocket size
+
