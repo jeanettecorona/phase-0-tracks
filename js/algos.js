adcbd8505340548621a070cc9a3ef61fff12cef2
Jeanette Corona <jscarlettcorona@gmail.com>
2016-08-29 11:02:06 -0700

    Create function to make array of random words and repeat 10 times

--- js/algos.js
+++ js/algos.js
@@ -21,7 +21,7 @@ for (var i = 0; i < words.length; i++) {
 }
 
 // Driver code
-console.log(longest_string(["str", "strr", "String"]));
+console.log(longest_string(["str", "strr", "string"]));
 
 
 // Release 1
@@ -44,8 +44,49 @@ function compareMatch (person, person2){
 	return false;
 }
 
-console.log(compareMatch(person,person2));
+// Driver Code
+// console.log(compareMatch(person,person2));
 
+// Release 2
 
+// Write a function that takes an integer for length
+	// Declare empty array to hold the array of random word
+	// Declare variable with alphabet
+// Build a new string using integer given
+	// Decare an empty string to store the new string
+	// Declare the empty string to equal a random number between 1-10
+	// Loop using the random number to build a random word
+	// Push the random word into the empty array
+
+function makeWordArray(arraynum) {
+  var wordArray = [];
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    
+    for (var i=0; i < arraynum; i++) {
+      var newWord = "";
+      var wordLength = Math.floor((Math.random() * 10) + 1);
+      
+      for (var j=0; j < wordLength; j++ ) {
+          newWord += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+      }
+      wordArray.push(newWord);
+    }
+    return wordArray;
+}
+
+// Driver Code
+// console.log(makeWordArray(3))
+
+// Add driver code that does the following 10 times: 
+	// generates an array, 
+	// prints the array, 
+	// feeds the array to your "longest word" function, 
+	// and prints the result.
+
+for (var i = 0; i < 10; i++){
+	array = makeWordArray(3)
+	console.log(array)
+	console.log(longest_string(array))
+}
 
 
